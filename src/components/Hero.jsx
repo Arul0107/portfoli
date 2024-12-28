315c09de02407b292825c704393e4ef458b5aeba
Arul0107 <arulprakshv0107@gmail.com>
2024-12-28 22:36:31 +0530

    resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Row, Col, Button } from 'antd'; // Import Ant Design components
-import './Hero.css';
+import React from "react";
+import { Row, Col, Button } from "antd"; // Import Ant Design components
+import "./Hero.css";
 // Import images
-import pattern from '../assets/images/hero/pattern.png';
-import longArrow from '../assets/images/hero/long-arrow.svg';
-import stylishArrow from '../assets/images/hero/style-arrow.svg';
-import profilePic from '../assets/images/hero/profile-picture.png';
-import star from '../assets/images/hero/star.svg';
+import pattern from "../assets/images/hero/pattern.png";
+import longArrow from "../assets/images/hero/long-arrow.svg";
+import stylishArrow from "../assets/images/hero/style-arrow.svg";
+import profilePic from "../assets/images/hero/profile-picture.png";
+import star from "../assets/images/hero/star.svg";
 
 const Hero = () => {
   return (
@@ -21,7 +21,11 @@ const Hero = () => {
 
                 <div className="social-media-wrapper">
                   <div className="social-button">
-                    <img className="long-arrow" src={longArrow} alt="long arrow" />
+                    <img
+                      className="long-arrow"
+                      src={longArrow}
+                      alt="long arrow"
+                    />
                     <a
                       href="https://www.linkedin.com/in/arulprakash-v-4b537826a/"
                       target="_blank"
@@ -48,13 +52,16 @@ const Hero = () => {
 
                 <div className="hero-title-heading-wrapper">
                   <h1>
-                    <span>I Design</span> & Build <br />Unique Visuals and Graphics
+                    <span>I Design</span> & Build <br />
+                    Unique Visuals and Graphics
                   </h1>
                 </div>
 
                 <div className="hero-btn-wrapper">
                   <Button className="theme-btn-one">Hy! I'm Arul</Button>
-                  <Button className="theme-btn-two">Download CV</Button>
+                  <a href="/ar resume.pdf" download="Arul_Resume.pdf">
+                    <Button className="theme-btn-two">Download CV</Button>
+                  </a>
                 </div>
               </div>
             </div>
@@ -63,8 +70,16 @@ const Hero = () => {
           {/* Right Side: Profile Image and Other Elements */}
           <Col xs={24} md={12} className="text-center">
             <div className="profile">
-              <img className="stylish-arrow" src={stylishArrow} alt="stylish arrow" />
-              <img className="profile-pic" src={profilePic} alt="profile picture" />
+              <img
+                className="stylish-arrow"
+                src={stylishArrow}
+                alt="stylish arrow"
+              />
+              <img
+                className="profile-pic"
+                src={profilePic}
+                alt="profile picture"
+              />
               <img className="stylish-star" src={star} alt="stylish star" />
             </div>
           </Col>
